0253f4b848455fd6d4a92bc60dc109c1db7274c5
Humayun-98 <mohammadhumayun1998@gmail.com>
2025-07-07 04:56:07 +0500

    Completed Quote Generator with ShadCN UI form, local JSON, and Tailwind

---
+++ assignment-1/src/app/data/quote.ts
@@ -0,0 +1,8 @@
+export const quotes = [
+  { topic: "life", text: "Life is what happens when you’re busy making other plans." },
+  { topic: "life", text: "Get busy living or get busy dying." },
+  { topic: "life", text: "The purpose of life is a life of purpose." },
+  { topic: "success", text: "Success is not in what you have, but who you are." },
+  { topic: "success", text: "Success usually comes to those too busy to be looking for it." },
+  { topic: "success", text: "Don’t be afraid to give up the good to go for the great." }
+];
